dd2495d1203795c5cd0dba516163f4fc07fe1cda
idreral <idreral012@gmail.com>
2026-08-20 21:10:02 +0300

    Added few pages

--- app.py
+++ app.py
@@ -14,7 +14,27 @@ ip = '127.0.0.1'
 def index():
     return render_template('index.html')
 
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/about-us')
+def about():
+    return render_template('about.html')
+
+@app.route('/bonus')
+def bonus():
+    return render_template('bonus.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
 if __name__ == '__main__':
  #   with app.app_context():
   #      db.create_all()
-        app.run(debug=True, host=ip, port='5001')
+        app.run(debug=False, host=ip, port='5001')
